97fe41dc11276f0c8d59f720e78feba06b0756f9
Jablan <joshuaablan@gmail.com>
2019-03-08 00:23:30 -0800

    cleaned up code and added functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,28 @@
 var cards = ["queen", "queen", "king", "king"];
+
 var cardsInPlay = [];
-var cardOne = cards[0];
-	cardsInPlay.push(cardOne);
-var cardTwo = cards[2];
-	cardsInPlay.push(cardTwo)
- if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  		console.log("You found a match!");
+	} else {
+  		console.log("Sorry, try again.");
+	}
+}
+var flipCard = function (cardID) {
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+	
+	/*if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
  	alert("You found a match!");
- } else {
+ 	} else {
  	alert("Sorry, try again");
- }
+ 	}*/
+}
+
+flipCard(0);
+flipCard(2);
+
